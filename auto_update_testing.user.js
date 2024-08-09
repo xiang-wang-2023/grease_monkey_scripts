246c419ec6469fde4dda3c7f997e69725d8d3804
xiang-wang-2023 <120538839+xiang-wang-2023@users.noreply.github.com>
2024-08-08 20:12:02 -0700

    Update auto_update_testing.user.js

--- auto_update_testing.user.js
+++ auto_update_testing.user.js
@@ -2,6 +2,18 @@
 // @name     Au-to updating testing
 // @version  1
 // @updateURL  https://github.com/xiang-wang-2023/grease_monkey_scripts/blob/main/auto_update_testing.user.js
+// @match        *://*/*
 // ==/UserScript==
 
-console.log("hello world");
+// console.log("hello world");
+
+(function() {
+    'use strict';
+
+    // 创建一个新窗口
+    let newWindow = window.open('', '_blank', 'width=300,height=200');
+
+    // 设置新窗口的内容
+    newWindow.document.write('<html><head><title>Hello World</title></head><body><h1>Hello, World!</h1></body></html>');
+    newWindow.document.close();
+})();
