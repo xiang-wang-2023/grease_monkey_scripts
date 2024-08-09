e7d586ab386dab6ed08928c674762429a420290b
xiang-wang-2023 <120538839+xiang-wang-2023@users.noreply.github.com>
2024-08-08 20:20:14 -0700

    Update auto_update_testing.user.js

--- auto_update_testing.user.js
+++ auto_update_testing.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name     Au-to updating testing
-// @version  3
+// @version  4
 // @updateURL  https://github.com/xiang-wang-2023/grease_monkey_scripts/blob/main/auto_update_testing.user.js
-// @include      /^https?:\/\/baidu\.net\/.*$/
-// @match        *://*/*
+// @include      /^https?:\/\/baidu\.com\/.*$/
 // ==/UserScript==
 
 // console.log("hello world");
@@ -12,7 +11,7 @@
     'use strict';
 
     // 创建一个新窗口
-    let newWindow = window.open('', '_blank', 'width=300,height=200');
+    let newWindow = window.open('', '_blank', 'width=500,height=500');
 
     // 设置新窗口的内容
     newWindow.document.write('<html><head><title>Hello World</title></head><body><h1>Hello, World!</h1></body></html>');
