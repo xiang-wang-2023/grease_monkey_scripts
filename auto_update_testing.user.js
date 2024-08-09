59bb488f88ba4ef10cd519182e50971858e1d102
xiang-wang-2023 <120538839+xiang-wang-2023@users.noreply.github.com>
2024-08-08 20:31:52 -0700

    Update auto_update_testing.user.js

--- auto_update_testing.user.js
+++ auto_update_testing.user.js
@@ -13,6 +13,8 @@
     // 创建一个新窗口
     let newWindow = window.open();
 
+    
+
     // 设置新窗口的内容
     newWindow.document.write('<html><head><title>Hello World</title></head><body><h1>Hello, World!</h1></body></html>');
     newWindow.document.close();
