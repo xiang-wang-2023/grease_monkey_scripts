159c9d8df8fd5b0dc596ad73bbdbbe5854c96a26
xiang-wang-2023 <120538839+xiang-wang-2023@users.noreply.github.com>
2024-08-08 20:08:14 -0700

    Update auto_update_testing.user.js

--- auto_update_testing.user.js
+++ auto_update_testing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     Au-to updating testing
 // @version  1
-// @updateURL    
+// @updateURL  https://github.com/xiang-wang-2023/grease_monkey_scripts/blob/main/auto_update_testing.user.js
 // ==/UserScript==
 
 console.log("hello world");
